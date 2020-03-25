c193294e66cbef5e05603ec505379cf0bb8ebdce
lqqokim <kis420404@gmail.com>
2020-03-25 17:48:56 +0900

    feat: add redux

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { createStore } from 'redux';
+
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
+
+const countModifier = (state = 0) => {
+    return state;
+};
+const countStore = createStore(countModifier);
+
+console.log(countStore.getState());
+
+// let count = 0;
+// number.innerText = count;
+
+// const updateText = () => {
+//     number.innerText = count;
+// }
+
+// const handleAdd = () => {
+//     count = count + 1;
+//     updateText();
+// }
+
+// const handleMinus = () => {
+//     count = count - 1;
+//     updateText();
+// }
+
+// add.addEventListener('click', handleAdd);
+// minus.addEventListener('click', handleMinus);
